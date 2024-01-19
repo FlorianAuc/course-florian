72c459e25c40c8b6e11c1b1993164898bd81aed7
FlorianAuc <florian.aucouturier@gmail.com>
2024-01-19 10:25:34 +0100

    first commit

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,161 @@
+<template>
+  <div class="container content">
+    <header class="hero is-primary">
+      <div class="hero-body">
+        <h1 class="title">
+          <img style="height: 36px" src="/src/assets/images/logo.png" /> Je cours...
+        </h1>
+        <p class="settings"><i class="fas fa-cog"></i></p>
+      </div>
+    </header>
+    <section class="main-content container content">
+      <div class="objectif"></div>
+      <p><button class="button start is-danger">Démarrer</button></p>
+      <div class="encours">
+        <h2 class="jour title"></h2>
+        <p class="progress-text"></p>
+        <p class="vitesse"></p>
+        <p class="action"></p>
+        <audio src=""></audio>
+        <progress class="progress is-large is-success" max="13" value2="0">0</progress>
+      </div>
+      <div class="legend">
+        <h3>Légende</h3>
+        <table class="table is-striped">
+          <tr>
+            <td>Echauffement</td>
+            <td>
+              Ensemble de
+              <a
+                href="https://www.youtube.com/watch?v=hijkKi4g_q8&ab_channel=MatthieuVerneret"
+                target="_blank"
+                >petits exercices</a
+              >
+              assez doux
+            </td>
+          </tr>
+          <tr>
+            <td>Trot</td>
+            <td>Course normale</td>
+          </tr>
+          <tr>
+            <td>Trot lent</td>
+            <td>Limite marche rapide</td>
+          </tr>
+          <tr>
+            <td>Déboulé</td>
+            <td>Accélération constante sur 100 à 200 m.</td>
+          </tr>
+          <tr>
+            <td>Course</td>
+            <td>Trot à vitesses variables</td>
+          </tr>
+          <tr>
+            <td>Sprint</td>
+            <td>Vitese max</td>
+          </tr>
+          <tr>
+            <td>Etirements</td>
+            <td>
+              Ensemble de
+              <a
+                href="https://www.youtube.com/watch?v=rlDv7M_MxLw&ab_channel=LucileWoodward"
+                target="_blank"
+                >petits exercices</a
+              >
+              assez doux
+            </td>
+          </tr>
+        </table>
+      </div>
+    </section>
+    <footer class="footer">
+      <p>
+        &copy; l'R & L'O.be - {{ year }} -
+        <a
+          href="https://jogging.jograph.be/les-entrainements-je-cours-pour-ma-forme/"
+          target="_blank"
+          >Inspired</a
+        >
+      </p>
+      <button class="button reset is-danger">Reset</button>
+      <button class="button reset-day is-danger">Reset day</button>
+      <button class="button update is-warning">Update</button>
+      <button class="button install is-primary"><i class="fas fa-download"></i></button>
+    </footer>
+  </div>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue';
+
+const year = ref(new Date().getFullYear());
+
+// Fonction pour mettre à jour l'année
+const updateYear = () => {
+  year.value = new Date().getFullYear();
+};
+
+// Surveiller les changements de l'année
+watch(() => new Date().getFullYear(), updateYear);
+
+// Appeler la fonction pour initialiser l'année
+updateYear();
+</script>
+
+<style lang="scss">
+.encours {
+  display: none;
+}
+section.container {
+  padding: 1rem;
+}
+.install {
+  display: none;
+  float: right;
+}
+.update {
+  display: none;
+  float: left;
+  margin-right: 1em;
+}
+.reset {
+  float: left;
+  margin-right: 1em;
+}
+.reset-day {
+  float: left;
+  margin-right: 1em;
+}
+.main-content {
+  height: calc(100vh - 320px) !important;
+}
+.footer {
+  height: 80px !important;
+}
+.table {
+  td {
+    font-size: 11px;
+  }
+}
+.settings {
+  position: absolute;
+  right: 0;
+  top: 0;
+  margin-top: 1rem;
+  margin-right: 1rem;
+  font-size: 2rem;
+}
+.objectif {
+  margin-bottom: 1rem;
+}
+@media screen and (min-width: 640px) {
+  .encours {
+    float: left;
+  }
+  .legend {
+    max-width: 50%;
+    float: right;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <HomeComponent></HomeComponent>
+  </main>
+</template>
+
+<script setup>
+import HomeComponent from '@/components/HomeComponent.vue'
+</script>
+
+<style lang="scss"></style>
